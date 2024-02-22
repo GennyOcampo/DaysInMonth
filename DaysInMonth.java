1ce427ee194d1d4c34401677697b360c7057c07e
GennyOcampo <ogeneiva10@gmail.com>
2024-02-22 10:03:30 -0600

    Update DaysInMonth.java
    case added

--- DaysInMonth.java
+++ DaysInMonth.java
@@ -15,20 +15,34 @@ public class DaysInMonth {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int startingValue ;
+        int startingValue =1 ;
         for (int month = 0; month<= startingValue; month += 1;)
         {
-        switch(month);
-                {
-            case 9:
+        switch(month)
+        {
             case 4:
             case 6:
-            case 11:
+            case 8:
+            case 10:
+            case 12:
                 System.out.println("30 days");
                 break;
-                
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+            case 11:
+                System.out.println("31 days");
+                break;
+            case 2:
+                if (month == 2){
+                    System.out.println("28 days unless it is on leap year");
+                }
+                break;
         }
         }
+        System.out.printf("month" + month);
     }
     
 }
