7f4f161ab6567ae968c98cf029a47fd272fbffb6
GennyOcampo <ogeneiva10@gmail.com>
2024-02-22 09:43:34 -0600

    Add files via upload

---
+++ DaysInMonth.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daysinmonth;
+
+/**
+ *
+ * @author OcampoG4446
+ */
+public class DaysInMonth {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int startingValue ;
+        for (int month = 0; month<= startingValue; month += 1;)
+        {
+        switch(month);
+                {
+            case 9:
+            case 4:
+            case 6:
+            case 11:
+                System.out.println("30 days");
+                break;
+                
+        }
+        }
+    }
+    
+}
